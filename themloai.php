ec6768a1f792245523c9e75ddbd8984c7a5102bf
sideptraihihi <sitrandeptrai@gmail.com>
2019-04-08 16:43:06 +0700

    fix commit

--- themloai.php
+++ themloai.php
@@ -8,6 +8,7 @@
 </head>
 <?php
     include ("connect.php");
+
     if(isset($_POST['btnthem']))
     {
      
@@ -15,16 +16,37 @@
         $resurel_loai =  mysqli_query($connect,$sql_loai);
         // header
     }
-    
+    if(isset($_POST['btnsua']))
+    {
+        
+    $sql_sua=  "UPDATE `loai` SET `tenloai`='".$_POST['txtsua']."' WHERE `maloai` =".$_POST['txtloai']." ;";
+        
+        // echo $sql_sua;
+        $resurel_sua =  mysqli_query($connect,$sql_sua);
+        // header
+    }
+
+    include  ("select.php");
 ?>
 <body>
 <form action="" method="post">
     loai san pham : 
    <input type="text" name="txtloai" id="">
+    sua san pham : 
+   <input type="text" name="txtsua" id="">
    <input type="submit" value = "btnthem" name="btnthem" id="">
+   <input type="submit" value = "btnSua" name="btnsua" id="">
+   <?php
+while ($row1  = mysqli_fetch_array($loai)) {  ?>
+<?php  echo "<p>$row1[0]   $row1[1]</p>"   ?>
+  
 
+ <?php } 
+?>
 
 </form>
+
+
 </body>
     
 </body>
